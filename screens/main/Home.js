9fd74ea08d3c0736239b82ecd62f9fcf78ccec3c
권수열 <>
2022-08-25 23:16:09 +0900

    home 화면 로고 위치조정, Search Bar 구현

--- screens/main/Home.js
+++ screens/main/Home.js
@@ -1,57 +1,61 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import styled from 'styled-components/native';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, Feather } from '@expo/vector-icons';
 import utils from '../../utils';
+import { TextInput } from 'react-native-gesture-handler';
 
 const Container = styled.View`
   flex: 1
-  background-color: #fff;
-  align-items: center;
+  background-color: #68c2ff;
+  align-items: between;
   justify-content: flex-start;
 `;
 
 const HeadContainer = styled.View`
   height: 20%;
-  width: 90%;
-  justify-content: center;
-  align-items: center;
+  width: 95%;
+  padding-top: 5%;
+  justify-content: space-around;
+  flex-direction: row;
 `;
 
 const MainLogo = styled.Image`
-  width: 170px;
+  width: 180px;
   height: 50px;
+  margin: auto;
 `;
 
 const MainLogoView = styled.View``;
 
 const SearchContainer = styled.View`
   flex-direction: row;
-  margin-left: auto;
-  margin-top: 20px;
+  width: 50%;
+  margin: auto;
+  margin-left: 1%;
 `;
 
-const SearchPressable = styled.Pressable`
-  padding: 5px;
+const SearchBar = styled.TextInput`
+  width: 90%;
+  height: 30px;
+  border: solid;
+  border-radius: 10px;
+  background-color: #fff;
 `;
 
 const Home = () => {
   const navigation = useNavigation();
+  const [text, onChangeText] = React.useState("검색어를 입력하시오.");
   return (
     <Container>
       <HeadContainer>
-        <SearchContainer>
-          <SearchPressable onPress={() => navigation.navigate('Search')}>
-            <Ionicons
-              name={utils.isAndroid() ? 'md-search' : 'ios-search'}
-              size={30}
-              color="black"
-            />
-          </SearchPressable>
-        </SearchContainer>
         <MainLogoView>
           <MainLogo source={require('../../assets/logo.png')} />
         </MainLogoView>
+        <SearchContainer>
+          <Feather name="search" size={25} color="black"/>
+          <SearchBar onChangeText={onChangeText} value={text}/>
+        </SearchContainer>
       </HeadContainer>
     </Container>
   );
