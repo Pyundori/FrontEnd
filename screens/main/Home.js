355b884726d64a1c9b3526f5cdcd44209bade869
권수열 <>
2022-08-25 23:16:43 +0900

    Revert "home 화면 로고 위치조정, Search Bar 구현"
    This reverts commit 9fd74ea08d3c0736239b82ecd62f9fcf78ccec3c.

--- screens/main/Home.js
+++ screens/main/Home.js
@@ -1,61 +1,57 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import styled from 'styled-components/native';
-import { Ionicons, Feather } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 import utils from '../../utils';
-import { TextInput } from 'react-native-gesture-handler';
 
 const Container = styled.View`
   flex: 1
-  background-color: #68c2ff;
-  align-items: between;
+  background-color: #fff;
+  align-items: center;
   justify-content: flex-start;
 `;
 
 const HeadContainer = styled.View`
   height: 20%;
-  width: 95%;
-  padding-top: 5%;
-  justify-content: space-around;
-  flex-direction: row;
+  width: 90%;
+  justify-content: center;
+  align-items: center;
 `;
 
 const MainLogo = styled.Image`
-  width: 180px;
+  width: 170px;
   height: 50px;
-  margin: auto;
 `;
 
 const MainLogoView = styled.View``;
 
 const SearchContainer = styled.View`
   flex-direction: row;
-  width: 50%;
-  margin: auto;
-  margin-left: 1%;
+  margin-left: auto;
+  margin-top: 20px;
 `;
 
-const SearchBar = styled.TextInput`
-  width: 90%;
-  height: 30px;
-  border: solid;
-  border-radius: 10px;
-  background-color: #fff;
+const SearchPressable = styled.Pressable`
+  padding: 5px;
 `;
 
 const Home = () => {
   const navigation = useNavigation();
-  const [text, onChangeText] = React.useState("검색어를 입력하시오.");
   return (
     <Container>
       <HeadContainer>
+        <SearchContainer>
+          <SearchPressable onPress={() => navigation.navigate('Search')}>
+            <Ionicons
+              name={utils.isAndroid() ? 'md-search' : 'ios-search'}
+              size={30}
+              color="black"
+            />
+          </SearchPressable>
+        </SearchContainer>
         <MainLogoView>
           <MainLogo source={require('../../assets/logo.png')} />
         </MainLogoView>
-        <SearchContainer>
-          <Feather name="search" size={25} color="black"/>
-          <SearchBar onChangeText={onChangeText} value={text}/>
-        </SearchContainer>
       </HeadContainer>
     </Container>
   );
