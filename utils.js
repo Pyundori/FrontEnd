e793bf44e3bd510037b3f4533a49572a205d3ccc
YongjaeHyun <dltjrrbs2020@kumoh.ac.kr>
2022-09-05 11:57:59 +0900

    [AUTH 6] 회원가입 스크린 구현

--- utils.js
+++ utils.js
@@ -3,8 +3,16 @@ import { Platform } from 'react-native';
 export default {
   isAndroid: () => Platform.OS === 'android',
   isEmail: (email) => {
-    const regEx =
+    const regExp =
       /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-    return regEx.test(email);
+    return regExp.test(email);
+  },
+  isId: (id) => {
+    const regExp = /^[a-zA-Z]+[a-z0-9]{5,19}$/g;
+    return regExp.test(id);
+  },
+  isPassword: (password) => {
+    const regExp = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
+    return regExp.test(password);
   },
 };
