c11db1cb54917ca8de731866100facf7b6be8546
Huze <dltjrrbs2020@gmail.com>
2022-09-21 02:09:28 +0900

    [NAV 9] 코드 정리가 안 되어있던 부분 정리

--- navigator/Gate.js
+++ navigator/Gate.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Main from './Main';
-import Auth from './Auth'
+import Auth from './Auth';
 import { useSelector } from 'react-redux';
 
 export default function Gate() {
-  const isLogined = useSelector((state) => state.users.isLogined)
-  return (
-    <NavigationContainer>
-      {isLogined ? <Main /> : <Auth />}
-    </NavigationContainer>
-  );
+  const isLogined = useSelector((state) => state.users.isLogined);
+  return <NavigationContainer>{isLogined ? <Main /> : <Auth />}</NavigationContainer>;
 }
