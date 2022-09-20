b1cef6cd4c1d00eca69bb3ccde7f5ce2f2d425af
Huze <dltjrrbs2020@gmail.com>
2022-09-21 02:15:53 +0900

    [REDUX 10] Production 환경이 아니라면 redux devTool이 작동하도록 설정

--- redux/store.js
+++ redux/store.js
@@ -28,6 +28,7 @@ const store = configureStore({
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
 export const persistor = persistStore(store);
