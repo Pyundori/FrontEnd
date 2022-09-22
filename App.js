57bec738414dd88fcedfbec1be3c1955560a5ef1
Huze <dltjrrbs2020@gmail.com>
2022-09-22 21:26:21 +0900

    [APP 2] useEffect 안의 함수를 밖으로 빼내, useEffect의 가독성을 높임

--- App.js
+++ App.js
@@ -55,42 +55,41 @@ const cacheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 export default function App() {
   const [isLoading, setIsLoading] = useState(false);
   const handleFinish = () => setIsLoading(true);
+  const loadAssets = async () => {
+    try {
+      const customFonts = {
+        sansBold: require('./assets/fonts/GmarketSansBold.otf'),
+        sansLight: require('./assets/fonts/GmarketSansLight.otf'),
+        sansMedium: require('./assets/fonts/GmarketSansMedium.otf'),
+        netmarbleB: require('./assets/fonts/netmarbleB.ttf'),
+        netmarbleL: require('./assets/fonts/netmarbleL.ttf'),
+        netmarbleM: require('./assets/fonts/netmarbleM.ttf'),
+      };
+      const images = [
+        require('./assets/search.png'),
+        require('./assets/AroundVender.png'),
+        require('./assets/google_login.png'),
+        require('./assets/google_login_pressed.png'),
+        require('./assets/kakao_login.png'),
+        require('./assets/not_image.png'),
+        require('./assets/logo.png'),
+        require('./assets/cu.png'),
+        require('./assets/emart24.png'),
+        require('./assets/gs25.png'),
+        require('./assets/seven_eleven.png'),
+      ];
+      const fonts = [Ionicons.font, customFonts];
+      const imagePromises = cacheImages(images);
+      const fontPromises = cacheFonts(fonts);
+      return Promise.all([...fontPromises, ...imagePromises]);
+    } catch (e) {
+      console.warn(e);
+    }
+  };
   useEffect(() => {
-    const loadAssets = async () => {
-      try {
-        const customFonts = {
-          sansBold: require('./assets/fonts/GmarketSansBold.otf'),
-          sansLight: require('./assets/fonts/GmarketSansLight.otf'),
-          sansMedium: require('./assets/fonts/GmarketSansMedium.otf'),
-          netmarbleB: require('./assets/fonts/netmarbleB.ttf'),
-          netmarbleL: require('./assets/fonts/netmarbleL.ttf'),
-          netmarbleM: require('./assets/fonts/netmarbleM.ttf'),
-        };
-        const images = [
-          require('./assets/search.png'),
-          require('./assets/AroundVender.png'),
-          require('./assets/google_login.png'),
-          require('./assets/google_login_pressed.png'),
-          require('./assets/kakao_login.png'),
-          require('./assets/not_image.png'),
-          require('./assets/logo.png'),
-          require('./assets/cu.png'),
-          require('./assets/emart24.png'),
-          require('./assets/gs25.png'),
-          require('./assets/seven_eleven.png'),
-        ];
-        const fonts = [Ionicons.font, customFonts];
-        const imagePromises = cacheImages(images);
-        const fontPromises = cacheFonts(fonts);
-        return Promise.all([...fontPromises, ...imagePromises]);
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        handleFinish();
-      }
-    };
     checkForUpdates();
     loadAssets();
+    handleFinish();
   }, []);
 
   return isLoading ? (
